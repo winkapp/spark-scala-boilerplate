251d37eccacbcb9e8961b94aab639ad26c6f999f
kalman <kal@wink.com>
2017-01-20 17:10:00 -0500

    extends the app object because that's kind of awesome and I didn't know you could

--- src/main/scala/com/wink/spark/Run.scala
+++ src/main/scala/com/wink/spark/Run.scala
@@ -7,14 +7,14 @@ import org.apache.spark.{SparkConf, SparkContext}
   * Feel free to change this any way you like - it just serves as a convenient entry for SBT and Spark
   * IF your job needs more than one entry point, you probably need more than one job
   */
-object Run {
-  def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("Your App Name Here")
-    val sc = new SparkContext(conf)
+object Run extends App {
+  val conf = new SparkConf().setAppName("Your App Name Here")
+  val sc = new SparkContext(conf)
 
-    val a = sc.parallelize(1 to Int.MaxValue)
+  val a = sc.parallelize(1 to Int.MaxValue)
 
-    println(a.count())
-    println(a.map(x => x + 1).filter(x => x % 2 == 0).take(150).toList)
-  }
+  println(a.count())
+  println(a.map(x => x + 1).filter(x => x % 21 == 0).take(150).toList)
+
+  sc.stop
 }
